725a2c94fc57a7d0a7c2d127f78e7bc2e9869dd0
youssef7agar <youssef7agar24@gmail.com>
2020-09-07 04:14:07 +0200

    did the UI for LoginActivity.kt

--- app/src/main/java/com/example/initiumsolutionsassignment/main/MainActivity.kt
+++ app/src/main/java/com/example/initiumsolutionsassignment/main/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.initiumsolutionsassignment.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.initiumsolutionsassignment.R
+import com.example.initiumsolutionsassignment.login.LoginActivity
+import com.example.initiumsolutionsassignment.register.RegisterActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        tv_register.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
